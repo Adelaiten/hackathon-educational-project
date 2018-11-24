c8172d99b415bb76b7edf7d083218f02032e5c91
Paprok <rafal.becla@gmail.com>
2018-11-24 19:14:55 +0100

    merged dev into dao

--- src/main/java/educational/hackathon/roleplay_school/dao/DAOAccounts.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/DAOAccounts.java
@@ -2,4 +2,5 @@ package educational.hackathon.roleplay_school.dao;
 
 public interface DAOAccounts {
 //    public Account getAccountsByNicknameAndPassword();
+
 }
--- src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOAccountsSQL.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOAccountsSQL.java
@@ -1,9 +1,28 @@
 package educational.hackathon.roleplay_school.dao.daoSQL;
 
+import educational.hackathon.roleplay_school.connectors.SQLConnector;
 import educational.hackathon.roleplay_school.dao.DAOAccounts;
 
-public class DAOAccountsSQL implements DAOAccounts {
-    public void getAccountsByNicknameAndPassword(String nickname, String password){
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class DAOAccountsSQL implements DAOAccounts {
+    private Connection connection;
+    public DAOAccountsSQL(Connection connection){
+        this.connection = connection;
     }
+    public void getAccountsByNicknameAndPassword(String nickname, String password) throws SQLException {
+        String sql = "SELECT * FROM accounts WHERE nick=? AND password=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setString(1, nickname);
+        ps.setString(2, password);
+        ResultSet resultSet = ps.executeQuery();
+        if(resultSet.next()){
+            resultSet.
+        }
+    }
+
+
 }
