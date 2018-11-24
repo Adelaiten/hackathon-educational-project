7c844ae14cc5e3fa7f17a7db89c391b0dacd6b0e
Paprok <rafal.becla@gmail.com>
2018-11-24 21:12:52 +0100

    implemented extractAccountFromResult

--- src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOAccountsSQL.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOAccountsSQL.java
@@ -7,13 +7,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 public class DAOAccountsSQL implements DAOAccounts {
     private Connection connection;
     public DAOAccountsSQL(Connection connection){
         this.connection = connection;
     }
-    public Account getAccountsByNicknameAndPassword(String nickname, String password) throws SQLException {
+    public Account getAccountsByNicknameAndPassword(String nickname, String password) throws SQLException, NoSuchElementException {
         String sql = "SELECT * FROM accounts WHERE nick=? AND password=?";
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setString(1, nickname);
@@ -21,12 +22,22 @@ public class DAOAccountsSQL implements DAOAccounts {
         ResultSet resultSet = ps.executeQuery();
         if(resultSet.next()){
             return extractAccountFromResult(resultSet);
+        } else {
+            throw new NoSuchElementException();
         }
     }
 
-    private Account extractAccountFromResult(ResultSet resultSet){
-
-        return null;
+    private Account extractAccountFromResult(ResultSet resultSet) throws SQLException{
+        Account account = new Account();
+        account.setIdAccount(resultSet.getInt("id_account"));
+        account.setName(resultSet.getString("name"));
+        account.setSurname(resultSet.getString("surname"));
+        account.setUsername(resultSet.getString("username"));
+        account.setEmail(resultSet.getString("email"));
+        account.setPasswordHash(resultSet.getString("passwd_hash"));
+        account.setSalt(resultSet.getString("salt"));
+        return account;
     }
+
 
 }
