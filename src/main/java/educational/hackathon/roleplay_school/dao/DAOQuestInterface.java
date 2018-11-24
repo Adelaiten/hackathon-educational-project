bdf89dde23830ba5b95fd9795d2f559b6788ba69
Adelaiten <karoltrzaska19@gmail.com>
2018-11-24 21:46:27 +0100

    getAllStudentsQuest method done

--- src/main/java/educational/hackathon/roleplay_school/dao/DAOQuestInterface.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/DAOQuestInterface.java
@@ -2,9 +2,10 @@ package educational.hackathon.roleplay_school.dao;
 
 import educational.hackathon.roleplay_school.models.Quest;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAOQuestInterface {
-    Quest getQuestById(int id);
-    List<Quest> getAllAccountsQuests(int accountsId);
+    Quest getQuestById(int id) throws SQLException;
+    List<Quest> getAllStudentQuests(int accountsId) throws SQLException;
 }
--- src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOQuest.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/daoSQL/DAOQuest.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DAOQuest implements DAOQuestInterface {
@@ -35,7 +36,34 @@ public class DAOQuest implements DAOQuestInterface {
     }
 
     @Override
-    public List<Quest> getAllAccountsQuests(int accountsId) {
-        return null;
+    public List<Quest> getAllStudentQuests(int accountsId) throws SQLException{
+        List<Integer> studentIdQuests = getStudentQuestsIds(accountsId);
+        List<Quest> questsList = new ArrayList<>();
+        for(Integer questId : studentIdQuests) {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * FROM quest WHERE id_quest = ?");
+            preparedStatement.setInt(1, questId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()) {
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                double amount = resultSet.getDouble("amount");
+                int questType = resultSet.getInt("quest_type");
+                Quest quest = new Quest(questId, name, description, amount, questType);
+                questsList.add(quest);
+            }
+        }
+         return questsList;
+    }
+
+    private List<Integer> getStudentQuestsIds(int accountsId) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT id_quest FROM done_quests WHERE id_student = ?");
+        preparedStatement.setInt(1, accountsId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Integer> codecoolerIdQuests = new ArrayList<>();
+        while(resultSet.next()) {
+            Integer idQuest = resultSet.getInt("id_quest");
+            codecoolerIdQuests.add(idQuest);
+        }
+        return codecoolerIdQuests;
     }
 }
