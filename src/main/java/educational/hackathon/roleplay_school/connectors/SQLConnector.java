81dd409e83e9e154d17d83dd8ee612eb5166d473
Paprok <rafal.becla@gmail.com>
2018-11-24 16:16:18 +0100

    implemented Connector to SQL

---
+++ src/main/java/educational/hackathon/roleplay_school/connectors/SQLConnector.java
@@ -0,0 +1,29 @@
+package educational.hackathon.roleplay_school.connectors;
+
+import java.sql.*;
+
+public class SQLConnector {
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try {
+                createConnection();
+            } catch (SQLException e) {
+                System.out.println("Couldn't create connection to database");
+            }
+        } return connection;
+    }
+
+    private static void createConnection() throws SQLException {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/school", "queststore", "admin123");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+}
