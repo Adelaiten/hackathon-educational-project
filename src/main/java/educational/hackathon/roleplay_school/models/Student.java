7746eeed00fe31c53987feedffc4c3fd837ae951
Adelaiten <karoltrzaska19@gmail.com>
2018-11-24 19:03:41 +0100

    Student model

---
+++ src/main/java/educational/hackathon/roleplay_school/models/Student.java
@@ -0,0 +1,29 @@
+package educational.hackathon.roleplay_school.models;
+
+public class Student extends User {
+    private int idClass;
+    private int idStudent;
+
+    public Student(int idAccount, int idStudent, String username, String passwordHash, String name, String surname, String email, int role, int idClass) {
+        super(idAccount, username, passwordHash, name, surname, email, role);
+        this.idClass = idClass;
+        this.idStudent = idStudent;
+    }
+
+    public int getIdClass() {
+        return idClass;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public void setIdClass(int idClass) {
+        this.idClass = idClass;
+    }
+
+}
