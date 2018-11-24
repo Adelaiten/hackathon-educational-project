b8c9a24bedfa2ddd8bb10762baed52de7d57ad2f
Paprok <rafal.becla@gmail.com>
2018-11-24 21:53:49 +0100

    implemented methods in dao

--- src/main/java/educational/hackathon/roleplay_school/dao/DAOAccounts.java
+++ src/main/java/educational/hackathon/roleplay_school/dao/DAOAccounts.java
@@ -1,6 +1,10 @@
 package educational.hackathon.roleplay_school.dao;
 
-public interface DAOAccounts {
-//    public Account getAccountsByNicknameAndPassword();
+import educational.hackathon.roleplay_school.models.Account;
 
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+public interface DAOAccounts {
+    public Account getAccountsByNicknameAndPassword(String nickname, String password) throws SQLException, NoSuchElementException;
 }
