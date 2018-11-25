ba1844a5ac793458cdbdb42eb720de6692d228e9
Paprok <rafal.becla@gmail.com>
2018-11-25 08:57:41 +0100

    deleted usless mappers

--- src/main/java/educational/hackathon/roleplay_school/mappers/Mapper.java
+++
@@ -1,9 +0,0 @@
-package educational.hackathon.roleplay_school.mappers;
-
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface Mapper<E> {
-    E map(ResultSet resultSet) throws SQLException;
-}
