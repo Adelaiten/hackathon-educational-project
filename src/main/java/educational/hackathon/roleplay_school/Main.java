55f73fa35b84643fb63641804af06438023545c7
Adelaiten <karoltrzaska19@gmail.com>
2018-11-25 03:24:52 +0100

    merge commit

--- src/main/java/educational/hackathon/roleplay_school/Main.java
+++ src/main/java/educational/hackathon/roleplay_school/Main.java
@@ -1,15 +1,23 @@
 package educational.hackathon.roleplay_school;
 
 import educational.hackathon.roleplay_school.connectors.SQLConnector;
-import educational.hackathon.roleplay_school.dao.daoSQL.DAOAccountsSQL;
-import educational.hackathon.roleplay_school.models.Account;
+import educational.hackathon.roleplay_school.dao.daoSQL.DAOClassesSQL;
+import educational.hackathon.roleplay_school.dao.daoSQL.DAOQuest;
+import educational.hackathon.roleplay_school.models.ClassRoom;
+import educational.hackathon.roleplay_school.models.Quest;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection connection = SQLConnector.getConnection();
-        DAOAccountsSQL daoAccountsSQL = new DAOAccountsSQL(connection);
-        Account account = daoAccountsSQL.getAccountsByNicknameAndPassword(, )
+        DAOQuest daoQuest = new DAOQuest(connection);
+        List<Quest> daoQuestList = daoQuest.getAllStudentQuests(1);
+        for(Quest classRoom : daoQuestList) {
+            System.out.println(classRoom.getName() + " " + classRoom.getAmount() + " " + classRoom.getAmount() + " " + classRoom.getQuestType() );
+
+        }
     }
 }
