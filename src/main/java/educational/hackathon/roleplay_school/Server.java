88ef432cc96b5894943b0c80db35d2fbb57ca524
adrianOstromecki <bloodrian@gmail.com>
2018-11-25 02:55:58 +0100

    student profile jtwig renders data from model!

--- src/main/java/educational/hackathon/roleplay_school/Server.java
+++ src/main/java/educational/hackathon/roleplay_school/Server.java
@@ -1,6 +1,7 @@
 package educational.hackathon.roleplay_school;
 
 import com.sun.net.httpserver.HttpServer;
+import educational.hackathon.roleplay_school.dao.AllDAOs;
 import educational.hackathon.roleplay_school.handlers.LoginPageHandler;
 import educational.hackathon.roleplay_school.handlers.Static;
 import educational.hackathon.roleplay_school.helpers.CookieHelper;
@@ -9,11 +10,17 @@ import educational.hackathon.roleplay_school.httpHandlers.student.StudentProfile
 import java.net.InetSocketAddress;
 
 public class Server {
+    private AllDAOs allDAOs;
+
+    public Server(AllDAOs allDAOs){
+        this.allDAOs = allDAOs;
+    }
+
     public void run() {
         try {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(5777), 0);
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
             httpServer.createContext("/", new LoginPageHandler());
-            httpServer.createContext("/studentProfile", new StudentProfile(new CookieHelper("")));
+            httpServer.createContext("/studentProfile", new StudentProfile(allDAOs, new CookieHelper("")));
             httpServer.createContext("/static", new Static());
             httpServer.setExecutor(null);
             httpServer.start();
