e4990132656c11548867a9e50097a26eabb4aaff
Paprok <rafal.becla@gmail.com>
2018-11-25 02:27:27 +0100

    fixed connection do DB

--- src/main/java/educational/hackathon/roleplay_school/Server.java
+++ src/main/java/educational/hackathon/roleplay_school/Server.java
@@ -1,18 +1,29 @@
 package educational.hackathon.roleplay_school;
 
 import com.sun.net.httpserver.HttpServer;
+import educational.hackathon.roleplay_school.connectors.SQLConnector;
+import educational.hackathon.roleplay_school.dao.DAOAccounts;
+import educational.hackathon.roleplay_school.dao.DAOSession;
+import educational.hackathon.roleplay_school.dao.daoSQL.DAOAccountsSQL;
+import educational.hackathon.roleplay_school.dao.daoSQL.DAOSessionSQL;
 import educational.hackathon.roleplay_school.handlers.LoginPageHandler;
 import educational.hackathon.roleplay_school.handlers.Static;
 import educational.hackathon.roleplay_school.helpers.CookieHelper;
+import educational.hackathon.roleplay_school.httpHandlers.LoginHandler;
 import educational.hackathon.roleplay_school.httpHandlers.student.StudentProfile;
 
 import java.net.InetSocketAddress;
+import java.sql.Connection;
 
 public class Server {
     public void run() {
         try {
-            HttpServer httpServer = HttpServer.create(new InetSocketAddress(5777), 0);
-            httpServer.createContext("/", new LoginPageHandler());
+            Connection connection = SQLConnector.getConnection();
+            CookieHelper cookieHelper = new CookieHelper("cookie");
+            DAOSession daoSession = new DAOSessionSQL(cookieHelper);
+            DAOAccounts daoAccounts = new DAOAccountsSQL(connection);
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+            httpServer.createContext("/", new LoginHandler(daoAccounts, daoSession, cookieHelper));
             httpServer.createContext("/studentProfile", new StudentProfile(new CookieHelper("")));
             httpServer.createContext("/static", new Static());
             httpServer.setExecutor(null);
