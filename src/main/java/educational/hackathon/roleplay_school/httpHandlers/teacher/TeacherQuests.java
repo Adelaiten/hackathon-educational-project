7afa1af615e44791cab2e5cd983cc9e6864752b7
adrianOstromecki <bloodrian@gmail.com>
2018-11-25 10:46:18 +0100

    add edit and show quests

---
+++ src/main/java/educational/hackathon/roleplay_school/httpHandlers/teacher/TeacherQuests.java
@@ -0,0 +1,71 @@
+package educational.hackathon.roleplay_school.httpHandlers.teacher;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import educational.hackathon.roleplay_school.dao.AllDAOs;
+import educational.hackathon.roleplay_school.helpers.CookieHelper;
+import educational.hackathon.roleplay_school.models.Account;
+import educational.hackathon.roleplay_school.models.Quest;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeacherQuests implements HttpHandler {
+    private AllDAOs allDAOs;
+    private CookieHelper cookieHelper;
+
+    public TeacherQuests(AllDAOs allDAOs, CookieHelper cookieHelper){
+        this.allDAOs = allDAOs;
+        this.cookieHelper = cookieHelper;
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String method = httpExchange.getRequestMethod();
+
+        if (method.equals("GET")){
+//            Optional<HttpCookie> cookie = cookieHelper.getSessionIdCookie(httpExchange);
+//            String sessionId = cookie.get().getValue();
+//            sessionId = sessionId.replace("\"", "");
+//
+//            if(appDAOs.getDAOAccounts().isValidUserType( sessionId, "ADMIN")) {
+//                createResponse(httpExchange, sessionId);
+//            } else {
+//                System.out.println("Unauthorized request for admin");
+//                httpExchange.getResponseHeaders().add("Location", "/");
+//                httpExchange.sendResponseHeaders(303, 0);
+//
+//            }
+            createResponse(httpExchange);
+        }
+    }
+
+    private void createResponse(HttpExchange httpExchange) throws IOException{
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("template/teacher/quests.twig");
+        JtwigModel model = JtwigModel.newModel();
+        Quest quest1 = new Quest(1, "Do homework", "Very long description lorem ipsum non omnis moriar " +
+                "hack yeah 2018 hackathooooon #nosleep", 20, 10.0, "");
+        Quest quest2 = new Quest(2, "Make christmas decorations", "HOHOHO!", 10, 13.0, "");
+        Quest quest3 = new Quest(3, "Just example", "Nice description here please!", 7, 7.0, "");
+        Quest quest4 = new Quest(4, "Just example", "Nice description here please!", 7, 7.0, "");
+        Quest quest5 = new Quest(5, "Just example", "Nice description here please!", 7, 7.0, "");
+        List<Quest> quests = new ArrayList<>();
+        quests.add(quest1);
+        quests.add(quest2);
+        quests.add(quest3);
+        quests.add(quest4);
+        quests.add(quest5);
+        model.with("quests", quests);
+//        model.with("userNickname", nickname);
+        String response = template.render(model);
+        httpExchange.sendResponseHeaders(200, 0);
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
